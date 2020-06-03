5b66c4d458c31ab11053757f7f8fd2d4ca43af32
Speth <s9794755@stud.uni-frankfurt.de>
2020-06-03 08:29:21 +0200

    random forest with 2 and 3 classes

--- RandomForestCalculation.R
+++ RandomForestCalculation.R
@@ -4,39 +4,12 @@ data <- read.csv(
   text = getURL('https://raw.githubusercontent.com/FrancaSp/DataScience_Project/master/Datensaetze/finale_daten.csv')
 )
 
-data <- read.csv('~/Documents/UNI/Master/Data_Science/Daten_Codes/Datensaetze/finale_daten.csv')
+#data <- read.csv('~/Documents/UNI/Master/Data_Science/Daten_Codes/Datensaetze/finale_daten.csv')
 
 data <- data[2:ncol(data)]
 
 summary(data$HousePrice.RateofChange)
 quantile(data$HousePrice.RateofChange, c(0.33, 0.66,  1))
-
-data$RateofChange <- NA
-
-index <- c(1:145)
-for (i in index){
-  if (data$HousePrice.RateofChange[i] <= quantile(data$HousePrice.RateofChange, 0.25))
-  {
-    data$RateofChange[i]<- 1
-  }
-  if (data$HousePrice.RateofChange[i] > quantile(data$HousePrice.RateofChange, 0.25) &
-      data$HousePrice.RateofChange[i] <= quantile(data$HousePrice.RateofChange, 0.5))
-  {
-    data$RateofChange[i]<- 2
-  }
-  if (data$HousePrice.RateofChange[i] > quantile(data$HousePrice.RateofChange, 0.5) &
-      data$HousePrice.RateofChange[i] <= quantile(data$HousePrice.RateofChange, 0.75))
-  {
-    data$RateofChange[i]<- 3
-  }
-  if (data$HousePrice.RateofChange[i] > quantile(data$HousePrice.RateofChange, 0.75) &
-      data$HousePrice.RateofChange[i] <= quantile(data$HousePrice.RateofChange, 1))
-  {
-    data$RateofChange[i]<- 4
-  }
-}
-data <- data[-10]
-
 
 # Try with 3 classes: 
 data$RateofChange <- NA
@@ -58,8 +31,48 @@ for (i in index){
   }
 }
 data <- data[-10] 
-  
+data$RateofChange <- as.factor(data$RateofChange)   
+
+library(caret)
+# Create features and target
+data <- na.roughfix(data)
+X <- data %>% 
+  select(Overall.rank,  GDP.per.capita, #Score,
+         Healthy.life.expectancy, Freedom.to.make.life.choices,
+         Generosity, Perceptions.of.corruption,  EmploymentRate,Job.Vacancy, #longterm_interestrate,
+         HousePrice.AvgIndex.oneYearago, HousePrice.AvgIndex.twoYearsago)
+y <- data$RateofChange
+
+# Split data into training and test sets
+set.seed(14)
+index <- caret::createDataPartition(y, p=0.75, list=FALSE)
+X_train <- X[ index, ]
+X_test <- X[-index, ]
+y_train <- y[index]
+y_test <- y[-index]
+
+# Train the model 
+regr3 <- randomForest(x = X_train, y = y_train, maxnodes = 100, ntree = 100, replace = TRUE)
+
+# Make prediction
+predictions <- predict(regr3, X_test)
+
+#result <- X_test
+#result['prediction'] <- y_test
+#result['prediction']<-  predictions
+#head(result)
+
+table(predictions,y_test) 
+confusionMatrix(predictions,y_test)
+regr3$importance
+
 # Try with two classes: 
+data <- read.csv(
+  text = getURL('https://raw.githubusercontent.com/FrancaSp/DataScience_Project/master/Datensaetze/finale_daten.csv')
+)
+data <- data[2:ncol(data)]
+data$RateofChange <- NA
+
 index <- c(1:145)
 for (i in index){
   if (data$HousePrice.RateofChange[i] <= quantile(data$HousePrice.RateofChange, 0.5))
@@ -75,23 +88,6 @@ for (i in index){
 data <- data[-10]
 
 data$RateofChange <- as.factor(data$RateofChange) 
-set.seed(100)
-train <- sample(nrow(data), 0.75*nrow(data), replace = FALSE)
-TrainSet <- data[train,]
-ValidSet <- data[-train,]
-
-
-# Create a Random Forest model with default parameters
-model1 <- randomForest(RateofChange ~ Overall.rank + Score +GDP.per.capita +
-                         Healthy.life.expectancy + Freedom.to.make.life.choices +
-                         Generosity + Perceptions.of.corruption + HousePrice.AvgIndex.oneYearago +
-                         HousePrice.AvgIndex.twoYearsago + Job.Vacancy + 
-                         EmploymentRate + longterm_interestrate, data = TrainSet, importance = TRUE)
-model1
-
-predTrain <- predict(model1, TrainSet[1:14], type = "class")
-# Checking classification accuracy
-table(predTrain, TrainSet$RateofChange)  
 
 library(caret)
 # Create features and target
@@ -112,10 +108,10 @@ y_train <- y[index]
 y_test <- y[-index]
 
 # Train the model 
-regr <- randomForest(x = X_train, y = y_train, maxnodes = 100, ntree = 100, replace = TRUE)
+regr2 <- randomForest(x = X_train, y = y_train, maxnodes = 100, ntree = 100, replace = TRUE)
 
 # Make prediction
-predictions <- predict(regr, X_test)
+predictions <- predict(regr2, X_test)
 
 #result <- X_test
 #result['prediction'] <- y_test
@@ -124,4 +120,4 @@ predictions <- predict(regr, X_test)
 
 table(predictions,y_test) 
 confusionMatrix(predictions,y_test)
-regr$importance
+regr2$importance
